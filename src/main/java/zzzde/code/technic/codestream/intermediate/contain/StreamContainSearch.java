fd6c39d847e1f1e59d2f1b0f9d06763c4befa29c
zhuhangxuan <zhuhangxuan@yugu.net.cn>
2024-01-26 16:32:58 +0800

    目录重构

--- src/main/java/zzzde/code/technic/codestream/intermediate/contain/StreamContainSearch.java
+++ src/main/java/zzzde/code/technic/codestream/intermediate/contain/StreamContainSearch.java
@@ -1,5 +1,7 @@
 package zzzde.code.technic.codestream.intermediate.contain;
 
+import cn.hutool.core.convert.Convert;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +13,7 @@ public class StreamContainSearch {
     public static void main(String[] args) {
         List<Map<String,Object>> list = new ArrayList<>();
         Map map = new HashMap();map.put("custName","1");map.put("custName2","2");
-        Map map2 = new HashMap();map2.put("custName","103");
+        Map map2 = new HashMap();map2.put("custName","203");
         Map map3 = new HashMap();map3.put("custName","123");
         list.add(map);
         list.add(map2);
@@ -20,7 +22,17 @@ public class StreamContainSearch {
         String containKey = "1";
         System.out.println(map.get("custName").toString().contains(containKey));
         List<Map> n = list.stream().filter(map1 -> map1.get("custName").toString().contains(containKey)).collect(Collectors.toList());
+        List<Map<String,Object>> list2 = new ArrayList();
         System.out.println(n);
+        List<Map<String,Object>> n2 = list2.stream().filter(x -> x.get("custName").toString().contains(containKey)).collect(Collectors.toList());
+        System.out.println(n2);
 
+//        Double similarityScore = Double.parseDouble("") / 100d;
+//        System.out.println(similarityScore);
+
+
+         if (0  == Convert.toInt("1",0)){
+             System.out.println("--------------------进入");
+         }
     }
 }
